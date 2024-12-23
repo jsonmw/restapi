b4c6059c33a8612e019d02e5349434715b4e9135
Jason Wild <wildj@wwu.edu>
2024-12-22 20:37:06 -0800

    Fixed CORS issue

--- src/main/java/com/jason/restapi/controller/AuthController.java
+++ src/main/java/com/jason/restapi/controller/AuthController.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,6 +28,7 @@ import jakarta.validation.Valid;
 @RestController
 @Slf4j
 @AllArgsConstructor
+@CrossOrigin("*")
 public class AuthController {
 
     private final ProfileService profileService;
