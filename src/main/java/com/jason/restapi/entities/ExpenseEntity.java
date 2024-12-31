ca036c26444be307dcff678f9de3eb0c0663c8df
Jason Wild <wildj@wwu.edu>
2024-12-31 14:35:59 -0800

    Mapped user to expenses and fixed service method logic

--- src/main/java/com/jason/restapi/entities/ExpenseEntity.java
+++ src/main/java/com/jason/restapi/entities/ExpenseEntity.java
@@ -5,13 +5,18 @@ import java.sql.Timestamp;
 import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -45,5 +50,10 @@ public class ExpenseEntity {
 
     @UpdateTimestamp
     private Timestamp updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private ProfileEntity owner;
 
 }
